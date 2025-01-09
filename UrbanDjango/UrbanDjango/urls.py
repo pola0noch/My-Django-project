26e47b36ce435fad5bbf9c2599c32c43aae0e0cc
pola0noch <klayriddell@gmail.com>
2025-01-09 17:24:43 +0300

    task3 finally

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -20,7 +20,7 @@ from task3.views import platform_view, games_view, cart_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('platform/', platform_view, name='platform'),
+    path('', platform_view, name='platform'),
     path('games/', games_view, name='games'),
     path('cart/', cart_view, name='cart')
 
